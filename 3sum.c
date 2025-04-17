600a1e86d629b4d3c0905ef4782c0a53573b3e3e
Salik <contact@salikkhan.com>
2025-04-17 12:35:22 +0530

    feat: add other operations of 3 numbers

--- 3sum.c
+++ 3sum.c
@@ -4,5 +4,8 @@ int main() {
     printf("Enter the three integers, separated by a comma: ");
     scanf("%d, %d, %d", &a, &b, &c);
     printf("The sum of %d, %d and %d is: %d\n", a, b, c, a + b + c);
+    printf("The product of %d, %d and %d is: %d\n", a, b, c, a * b * c);
+    printf("The difference of %d, %d and %d is: %d\n", a, b, c, a - b - c);
+    printf("The quotient of %d, %d and %d is: %f\n", a, b, c, (float)a / b / c);
     return 0;
 }
